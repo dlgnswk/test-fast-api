881e5c093695257884c22c3ac53b616e93be82f2
ljhoon <dlwogns5140@gmail.com>
2024-11-05 15:50:39 +0900

    Chore: python 기반 fastAPI 서버 기본 설정 완료

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# CORS 설정
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/hello")
+def read_root():
+    return {"message": "Hello from FastAPI"}
